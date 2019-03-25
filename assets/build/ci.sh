0d91aa94d469c6a2d34232f0f3ad59aadffcb271
unknown <gxggsrmyy@gmail.com>
2019-03-25 08:40:29 +0800

    添加标记

--- assets/build/ci.sh
+++ assets/build/ci.sh
@@ -66,7 +66,7 @@ function build_go() {
   curl -k https://storage.googleapis.com/golang/go1.4.3.linux-amd64.tar.gz | tar xz
   mv go goroot_bootstrap
 
-  git clone --branch master https://github.com/gxggsrmyy/go-1  go
+  git clone --branch master https://github.com/phuslu/go  go
   cd go/src
   # if [ "${GOTIP_FOLLOW}" = "true" ]; then
   # 	git remote add -f upstream https://github.com/golang/go
